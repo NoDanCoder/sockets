0f315cb41d1fe27b15795b659bc5024c828a3e40
Alejandro Robayo <1249@holbertonschool.com>
2019-12-26 18:06:14 +0000

    [Add] client - server code

---
+++ client.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+/* creeate socket */
+
+#define IPV4 AF_INET
+#define TCP SOCK_STREAM
+#define CONSTANTa 0
+
+/* set serv_addr struct */
+
+#define CONSTANTb AF_INET
+
+/* Handle error function */
+
+void error(char *msg)
+{
+	perror(msg);
+	exit(1);
+}
+
+int main(int ac, char *av[])
+{
+	int sockfd, port_num, bytes_sr;
+	
+	struct sockaddr_in serv_addr;
+
+	struct hostent *server;
+
+	char buffer[256];
+
+	/* check if hostname was given */
+
+	ac--;
+	if (ac != 2)
+		error("host address and hostname port please");
+
+	/* set port_number of server to connect */
+
+	port_num = atoi(av[2]);
+
+	/* Create note/socket */
+
+	sockfd = socket(IPV4, TCP, CONSTANTa);
+	if (sockfd < 0) 
+        	error("ERROR opening socket");
+
+	/* try to connect to given address and it will fill "server" struct
+	 * this is done to get the IP address
+	 */
+
+	server = gethostbyname(av[1]);
+	if (server == NULL)
+		error("ERROR no such host");
+
+	/* set to zero all values of the struct */
+
+        bzero((char *) &serv_addr, sizeof(serv_addr));
+
+	/* set ser_Addr struct */
+
+	serv_addr.sin_family = CONSTANTb;		/* it's a constant */
+	bcopy((char *) server->h_addr,			/* access to h_addr property of "server" and paste it */
+		(char *) &serv_addr.sin_addr.s_addr,	/* to serv_addr.sin_addr.s_addr */
+		server->h_length);			/* h_addr it's an alias of first element of h_addr_list property of "server" */
+	serv_addr.sin_port = htons(port_num);		/*  htons convert "little endian" to "big endian", next we set port number */
+
+	/* It try to connecto to given "serv_Addr" */
+	/* Notice that the client needs to know the port number of the
+	 * server, but it does not need to know its own port number.
+	 * This is typically assigned by the system when connect is called.
+	 */
+
+	if ( connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0 )
+		error("ERROR Conecting");
+
+	/*  */
+
+	printf("Please enter the message: ");
+	bzero(buffer, 256);
+	fgets(buffer, 255, stdin);
+	bytes_sr = write(sockfd, buffer, strlen(buffer));
+	if (bytes_sr < 0) 
+		error("ERROR writing to socket");
+	bzero(buffer,256);
+	bytes_sr = read(sockfd,buffer,255);
+	if (bytes_sr < 0) 
+		error("ERROR reading from socket");
+	printf("%s\n",buffer);
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+
+/* creeate socket */
+
+#define IPV4 AF_INET
+#define TCP SOCK_STREAM
+#define CONSTANTa 0
+
+/* set serv_addr struct */
+
+#define CONSTANTb AF_INET
+#define SERVER_ADDR INADDR_ANY
+
+/* Handle error function */
+
+void error(char *msg)
+{
+	perror(msg);
+	exit(1);
+}
+
+/* Main - function */
+
+int main(int ac, char *av[])
+{
+	int sockfd, newsockfd, port_num, cli_len, bytes_sr;
+
+	char buffer[256];
+	char ip[256];
+
+	struct sockaddr_in serv_addr, cli_addr;
+
+	/* check if port number was insert */
+
+	ac--;
+	if (ac != 1)
+		error("no port provided");
+
+	/* Create note/socket */
+
+	sockfd = socket(IPV4, TCP, CONSTANTa);
+	if (sockfd < 0)
+		error("ERROR opening socket");
+
+	/* set to zero all values of the struct */
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+
+	/* port number where the server start to listen for connections */
+
+	port_num = atoi(av[1]);
+
+	/* set serv_addr struct */
+
+	serv_addr.sin_family = CONSTANTb;		/* it's a constant */
+	serv_addr.sin_port = htons(port_num);		/* htons convert "little endian" to "big endian", next we set port number */
+	serv_addr.sin_addr.s_addr = SERVER_ADDR;		/* set ip address where the current server is running */
+
+	/* set conection parameters (serv_addr) to current process (sockfd) */
+
+	if ( bind( sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr) ) < 0 )
+		error("ERROR on binding");
+
+	/* allow to "sockfd" to recive conections and set max num of conection nodes */
+
+	listen(sockfd, 5);
+	printf("Mi ip address: %s\n", inet_ntoa(serv_addr.sin_addr));
+
+	/* stop process upto recive a successfull conection, comunicaction will be
+	 * handlen on the new socket "newsockfd", this does not kill "sockfd" process
+	 * and fill client info in "cli_addr"
+	 */
+
+	cli_len = sizeof(cli_addr);
+	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &cli_len);
+	if (newsockfd < 0)
+		error("ERROR on accept");
+
+	/* clean buffer, and wait for a message from a client, store it on the buffer */
+
+	bzero(buffer, 256);
+	bytes_sr = read(newsockfd, buffer, 256);
+	if (bytes_sr < 0)
+		error("ERROR on read from new socket");
+
+	/* print message */
+
+	printf("Here is the message: %s\n", buffer);
+
+	/*  */
+
+	bytes_sr = write(newsockfd, "Thats ok recived", 16);
+	if (bytes_sr < 0)
+		error("ERROR writing to socket");
+
+	/* Finish the program */
+
+	return 0;
+}
